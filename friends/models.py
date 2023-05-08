a6a9e8f96e9f31a26c16fb0aab0b3d80cdf34d53
YegorCrazy <egor2002288@gmail.com>
2023-05-09 01:02:58 +0300

    user model added and migrated

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    ID = models.AutoField(primary_key=True)
+    Username = models.CharField(max_length=30, unique=True)
+    TrueName = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.Username
