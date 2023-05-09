8d393498cc4cd9fad700fc09b8b3a4b9d4e673f6
YegorCrazy <egor2002288@gmail.com>
2023-05-09 13:45:00 +0300

    friendship and friendship request models added

--- friends/admin.py
+++ friends/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, FriendshipRequest, Friendship
 
 admin.site.register(User)
+admin.site.register(Friendship)
+admin.site.register(FriendshipRequest)
--- friends/models.py
+++ friends/models.py
@@ -9,3 +9,34 @@ class User(models.Model):
 
     def __str__(self):
         return self.Username
+
+
+class FriendshipRequest(models.Model):
+
+    Sender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='outcoming_requests',
+                               db_index=True)
+    Receiver = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='incoming_requests',
+                                 db_index=True)
+
+    def __str__(self):
+        return self.Sender.Username + '->' + self.Receiver.Username
+
+
+class Friendship(models.Model):
+
+    """
+    The idea is to store entry (1, 2) and (2, 1) for a friendship
+    between users 1 and 2. The other solution is to store only (1, 2)
+    and control if 1.ID < 2.ID, but SQLite doesn't provide this
+    """
+
+    Friend1 = models.ForeignKey(User, on_delete=models.CASCADE,
+                                related_name='friendships',
+                                db_index=True)
+    Friend2 = models.ForeignKey(User, on_delete=models.CASCADE,
+                                db_index=True)
+
+    def __str__(self):
+        return self.Friend1.Username + '+' + self.Friend2.Username
